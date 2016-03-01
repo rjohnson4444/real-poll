aa67f4cac058ff88ea0c92078c54fad64d5238aa
Ryan Johnson <rjohnson4444@live.com>
2016-03-01 15:24:55 -0700

    add basic server - add server file - add mocha for testing - add express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app     = express();
+
+module.exports = app;
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Real-Poll';
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
