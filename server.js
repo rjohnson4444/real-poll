97af7ec952ad106ed9642f2d5fdb016d11b7178c
Ryan Johnson <rjohnson4444@live.com>
2016-03-02 22:54:46 -0700

    add basic workflow of app - add 'links' page with voting link and admin link - add vote page for voting participants - add admin page for viewing results

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express    = require('express')
 const app        = express();
 const generateUrlId = require('./lib/generate-id');
+const bodyParser = require('body-parser');
 const voteUrlId    =  generateUrlId();
 const resultsUrlId =  generateUrlId();
 
@@ -9,18 +10,53 @@ module.exports = app;
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('static'));
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'jade');
 
 app.locals.title = 'Real-Poll';
-app.locals.pollParticipants = {}
+app.locals.pollTopic = {}
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get(`/${resultsUrlId}/results`, (req, res) => {
+    var id = resultsUrlId;
+    var currentPollTopic = app.locals.pollTopic[id]
+
+    res.render('results', { pollQuestion: currentPollTopic });
+});
+
+app.get(`/${voteUrlId}`, (req, res) => {
+    var id = resultsUrlId;
+    var currentPollTopic = app.locals.pollTopic[id]
+
+    res.render('vote', { pollQuestion: currentPollTopic });
+});
+
+app.post('/results', (req, res) => {
+    var id = resultsUrlId;
+    var currentPollTopic = app.locals.pollTopic[id]
+
+    // value that is returned from the radio button
+    console.log(req.body.vote)
+    res.render('vote', { pollQuestion: currentPollTopic });
+});
+
+app.post('/polls', (req, res) => {
+    var id = resultsUrlId;
+    app.locals.pollTopic[id] = req.body.pollTopic
+    // Initiate the 'polling' room
+
+    res.redirect('polls');
+});
+
 app.get('/polls', (req, res) => {
-    var voteUrl    = `https://real-polls/${voteUrlId}`
-    var resultsUrl = `https://real-polls/${resultsUrlId}/results`
+    var voteUrl    = `${req.protocol}://${req.get('host')}/${voteUrlId}`
+    var resultsUrl = `${req.protocol}://${req.get('host')}/${resultsUrlId}/results`
 
     res.render('pollGenerate', { voteUrl: voteUrl, resultsUrl: resultsUrl });
 });
