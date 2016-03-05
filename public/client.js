9d72a9b1f065a2a2f20e2be0617ec50ce64d5e70
Ryan Johnson <rjohnson4444@live.com>
2016-03-05 16:28:44 -0700

    remove console.log from index.js file

--- public/client.js
+++ public/client.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
     $('.add-input').on('click', (e) => {
-        console.log("hi")
         $('.choices').append("<br><br><input type='text' name='poll[choice][]' placeholder='Choice' class='form-control' ></input>")
     });
 });
